c370dce1ae7ea7e627dabb464b501c282ce30ddc
Saxbeck <saxbeck.patrick@gmail.com>
2022-05-18 18:25:17 +0200

    First commit, after create-reac-app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Layout, Menu } from 'antd'
+import { Content, Header } from 'antd/lib/layout/layout'
+
 function App() {
   return (
     <div className="App">
